4bcdc0f9508a5a31901fc541b983b8c51ec04e8e
vishnath <vishnusnathan@gmail.com>
2020-08-09 09:21:06 +0530

    Added Localstorage

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # todo
 Simple TODO list using jquery
-# Live demo
-https://dreamy-spence-36866d.netlify.app/
--- scripts.js
+++ scripts.js
@@ -1,8 +1,31 @@
 //<-- ===============jquery ===============----->
 
 		$(document).ready(function(){
-			var count=0; // for countig active lists
-			$("#add").click(function()
+			if(localStorage.getItem('todoList'))
+			 	// Getting list from local storage
+				var listContents=JSON.parse(localStorage.getItem('todoList'));
+			else
+				var listContents=[];
+			
+			if(listContents.length!=0){
+				$.each(listContents, function(index, value){
+					var listid=index;
+					console.log(value.completed,listid);
+					if(value.completed==false)
+					{	
+						
+						$('#thelist').append('<li class="li_notcompleted" id=' + listid + ' >' + '<input type="checkbox"> '+value.value + '\t'+'<button type="button" class="delete btn btn-danger btn-xs ">Delete</button>'+'</li>'); //adding list item
+					}
+					else{
+						$('#thelist').append('<li class="li_completed strike" id=' + listid + ' >' + '<input type="checkbox" checked> '+value.value + '\t'+'<button type="button" class="delete btn btn-danger btn-xs ">Delete</button>'+'</li>'); //adding list item
+	
+					}
+		
+				});
+			}
+		
+
+			$("#add").click(function(event)
 			{	
 
 				$('#ipget').focus(function() { // onfocus clearing of input field
@@ -17,31 +40,53 @@
 
 				}
 				else{
-					count++;
-					$('ul').append('<li class="li_notcompleted">' + '<input type="checkbox"> '+data + '\t'+'<button type="button" class="delete btn btn-danger btn-xs ">Delete</button>'+'</li>'); //adding list item
+					
+					var item={
+						completed: false,
+						value: data
+					}
+					listContents.push(item);
+					var listid=listContents.length-1;
+					// Adding item to list
+					$('#thelist').append('<li class="li_notcompleted" id=' + listid + ' >' + '<input type="checkbox"> '+data + '\t'+'<button type="button" class="delete btn btn-danger btn-xs ">Delete</button>'+'</li>'); //adding list item
 					$("#err").fadeOut('slow');
-					$("#count").text(count);
+					localStorage.setItem('todoList', JSON.stringify(listContents));
+					
+				
 				}
 			});
 
 			$(document).on("click", "input:checkbox", function(){ // for marking completed list item 
-
+				
+				var listid = $(this).parent().attr( 'id' );
+				
 				if($(this).prop("checked") == true){
-					count--;
-					$("#count").text(count);// showing active list item count
-					$(this).parent().addClass("strike");
-					$(this).parent().addClass("li_completed");
-					$(this).parent().removeClass("li_notcompleted");
 					
+					if($(this).parent().hasClass("strike")){
+						$(this).parent().removeClass("strike");
+						$(this).parent().removeClass("li_completed");
+						$(this).parent().addClass("li_notcompleted");
+						listContents[listid].completed=true;
+						localStorage.setItem('todoList', JSON.stringify(listContents));
+					}
+					else{
+						$(this).parent().addClass("strike");
+						$(this).parent().addClass("li_completed");
+						$(this).parent().removeClass("li_notcompleted");
+						listContents[listid].completed=true;
+						localStorage.setItem('todoList', JSON.stringify(listContents));
+					}
 				}
-				else if($(this).prop("checked") == false){
-					count++;
-					$("#count").text(count);// showing active list item count
-					$(this).parent().removeClass("strike");
-					$(this).parent().removeClass("li_completed");
-					$(this).parent().addClass("li_notcompleted");
-
-				}
+					else {
+						
+						$(this).parent().removeClass("strike");
+						$(this).parent().removeClass("li_completed");
+						$(this).parent().addClass("li_notcompleted");
+						listContents[listid].completed=false;
+						localStorage.setItem('todoList', JSON.stringify(listContents));
+					
+					}
+					
 			});
 			$("#all").click(function(){// for showing all list
 
@@ -61,13 +106,11 @@
 			$(document).on("click", ".delete", function(){ // for deleting a list element
 				
 				$(this).parent().hide(500);
-				if($(this).parent().hasClass("li_notcompleted")) // TO reduce active list count if it is not completed
-				{
-					count--;
-					$("#count").text(count);// showing active list item count
-				}
-				
+				var listid = $(this).parent().attr( 'id' )
+				// Removing item from list
+				listContents.splice(listid,1); 
 				$(this).parent().remove();
+				localStorage.setItem('todoList', JSON.stringify(listContents));
 			});
-			$("#count").text(count);// showing active list item count
+			
 		});
